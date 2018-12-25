3cac818ca0f1bf93141e713b0cc2f05c901c6f6e
ruchira peiris <ruchirapeiris7@gmail.com>
2018-12-25 16:48:24 +0530

    Refactor file name extract_comments -> code_analysis

--- Github/code_analysis/code_comment/code_analysis.py
+++ Github/code_analysis/code_comment/code_analysis.py
@@ -2,17 +2,15 @@ import re
 
 def lineCount(fileName):
     file = open(fileName,'r')
-    content = file.read()
+    content= file.read()
     pat = r'(/\*.*?\*/|//.*?$)'
-    lineCount = 0
     commentCount = 0
-    blankCount = 0
     codeCount = 0
 
     print content
     print '////////////////////////////////////////////////////////////'
     for comment in re.findall(pat,content,re.DOTALL|re.MULTILINE):
-            commentCount+=1
+            commentCount +=1
             print comment
 
 
