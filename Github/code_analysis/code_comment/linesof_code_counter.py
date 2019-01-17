21dc7bad7c7d5b72f1b3788bf03390e3478f8ff4
ruchira peiris <ruchirapeiris7@gmail.com>
2019-01-17 17:37:53 +0530

    get code-comment ratio for each user

--- Github/code_analysis/code_comment/linesof_code_counter.py
+++ Github/code_analysis/code_comment/linesof_code_counter.py
@@ -1,69 +1,83 @@
 
 import sys
 import os, os.path
+import csv
 # comment symbol
 commentSymbol = '#'
 acceptableFileExtensions = '.py'
 
-if not acceptableFileExtensions:
-    print 'File extension not found'
-    quit()
+def load_users():
+    with open('../../../Github_repos.csv','r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for user in csv_reader:
+            user_dir = '../../files/code_snippets/'+user[0]
+            if os.path.exists(user_dir):
+                currentDir = user_dir
+                code_comment_ratio(currentDir, user[0])
 
-currentDir = os.getcwd()
-print currentDir
+def code_comment_ratio(dir_to_check, user_to_check):
 
-filesToCheck = []
-for root, _, files in os.walk(currentDir):
-    for f in files:
-        fullpath = os.path.join(root, f)
-        if '.git' not in fullpath:
-            for extension in acceptableFileExtensions:
-            	if fullpath.endswith(extension):
-                    filesToCheck.append(fullpath)
+    if not acceptableFileExtensions:
+        print 'File extension not found'
+        quit()
 
-if not filesToCheck:
-    print 'No files found.'
-    quit()
+    print dir_to_check
 
-lineCount = 0
-totalBlankLineCount = 0
-totalCommentLineCount = 0
+    filesToCheck = []
+    for root, _, files in os.walk(dir_to_check):
+        for f in files:
+            fullpath = os.path.join(root, f)
+            if '.git' not in fullpath:
+                for extension in acceptableFileExtensions:
+                    if fullpath.endswith(extension):
+                        filesToCheck.append(fullpath)
 
-print ''
-print 'Filename\tlines\tblank lines\tcomment lines\tcode lines'
-# for loop
-for fileToCheck in filesToCheck:
-    with open(fileToCheck) as f:
+    if not filesToCheck:
+        print 'No files found.'
+        quit()
 
-        fileLineCount = 0
-        fileBlankLineCount = 0
-        fileCommentLineCount = 0
+    lineCount = 0
+    totalBlankLineCount = 0
+    totalCommentLineCount = 0
 
-        for line in f:
-            lineCount += 1
-            fileLineCount += 1
+    print ''
+    print 'Filename\tlines\tblank lines\tcomment lines\tcode lines'
+    # for loop
+    for fileToCheck in filesToCheck:
+        with open(fileToCheck) as f:
 
-            lineWithoutWhitespace = line.strip()
-            if not lineWithoutWhitespace:
-                totalBlankLineCount += 1
-                fileBlankLineCount += 1
-            elif lineWithoutWhitespace.startswith(commentSymbol):
-                totalCommentLineCount += 1
-                fileCommentLineCount += 1
+            fileLineCount = 0
+            fileBlankLineCount = 0
+            fileCommentLineCount = 0
 
-        print os.path.basename(fileToCheck) + \
-              "\t" + str(fileLineCount) + \
-              "\t" + str(fileBlankLineCount) + \
-              "\t" + str(fileCommentLineCount) + \
-              "\t" + str(fileLineCount - fileBlankLineCount - fileCommentLineCount)
+            for line in f:
+                lineCount += 1
+                fileLineCount += 1
 
-codeLines = lineCount - totalBlankLineCount - totalCommentLineCount
+                lineWithoutWhitespace = line.strip()
+                if not lineWithoutWhitespace:
+                    totalBlankLineCount += 1
+                    fileBlankLineCount += 1
+                elif lineWithoutWhitespace.startswith(commentSymbol):
+                    totalCommentLineCount += 1
+                    fileCommentLineCount += 1
 
-print ''
-print 'Totals'
-print '--------------------'
-print 'Lines:         ' + str(lineCount)
-print 'Blank lines:   ' + str(totalBlankLineCount)
-print 'Comment lines: ' + str(totalCommentLineCount)
-print 'Code lines:    ' + str(codeLines)
-print 'Code to comment Ratio:   '+str((float(totalCommentLineCount)/float(codeLines))*100)+' %'
+            print os.path.basename(fileToCheck) + \
+                  "\t" + str(fileLineCount) + \
+                  "\t" + str(fileBlankLineCount) + \
+                  "\t" + str(fileCommentLineCount) + \
+                  "\t" + str(fileLineCount - fileBlankLineCount - fileCommentLineCount)
+
+    codeLines = lineCount - totalBlankLineCount - totalCommentLineCount
+
+    print ''
+    print 'Totals'
+    print '--------------------'
+    print 'Lines:         ' + str(lineCount)
+    print 'Blank lines:   ' + str(totalBlankLineCount)
+    print 'Comment lines: ' + str(totalCommentLineCount)
+    print 'Code lines:    ' + str(codeLines)
+    print 'Code to comment Ratio of '+user_to_check+':  ' + str((float(totalCommentLineCount) / float(codeLines)) * 100) + ' %'
+
+
+load_users()
