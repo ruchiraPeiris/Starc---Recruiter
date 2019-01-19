3284e502f43a53226b4f5a329e14c7708d334be3
ruchira peiris <ruchirapeiris7@gmail.com>
2019-01-20 04:46:54 +0530

    retrieve words in readme file as a list

---
+++ Github/Github_Api/retrieve_readme.py
@@ -0,0 +1,31 @@
+import requests
+import csv
+import json
+import base64
+import nltk
+from nltk.tokenize import word_tokenize
+
+
+def words_list(userName):
+
+    repo_name = ''
+    with open('../../Github_repos.csv', 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+
+        for raw in csv_reader:
+            if raw[2] == userName:
+                repo_name = raw[3]
+                print repo_name
+    response = requests.get('https://api.github.com/repos/'+userName+'/'+repo_name+'/readme')
+    data = json.loads(response.content)
+    decoded_content = base64.b64decode(data["content"])
+    words = decoded_content.decode('utf-8')
+    word_tokens = word_tokenize(words)
+
+    filtered_sentence = []
+    for w in word_tokens:
+        filtered_sentence.append(w)
+
+
+    return filtered_sentence
+
