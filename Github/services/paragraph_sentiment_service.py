3bf278bbb99194eb678bbc5fd1a94d4a5bdef471
ruchira peiris <ruchirapeiris7@gmail.com>
2019-01-22 23:32:46 +0530

    UI,exception handling, remove punctuations from readme, add new spell checker service file

--- Github/services/paragraph_sentiment_service.py
+++ Github/services/paragraph_sentiment_service.py
@@ -8,12 +8,13 @@ with open('../../Github_repos.csv','r') as csv_file:
     csv_reader = csv.reader(csv_file)
 
     for user2 in csv_reader:
+
         list2 = []
-        list2 = commit_list(user2[0], user2[1], user2[2])
+        list2 = commit_list(user2[1], user2[2], user2[3])
         statement = ''
         score = 0
         for commit2 in list2:
             statement += commit2
         if len(list2) >= 5:
             score = calEmotionalLevel(statement)
-        print 'Value of ' + user2[1] + ' : ' + str(score)
+        print 'Value of ' + user2[0] + ' : ' + str(score)
